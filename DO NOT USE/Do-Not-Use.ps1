ecc0bf81a1d539fe89457722dd6bf4c5ff1df545
MathiasMSFT <mdumont@microsoft.com>
2022-07-27 22:17:33 -0400

    commit

--- DO NOT USE/Do-Not-Use.ps1
+++ DO NOT USE/Do-Not-Use.ps1
@@ -1,7 +1,2 @@
 Get-Service
 
-| Extension type        | Column1           | Stored        | Objects        | Format                               | SSO        |
-| -------------------- :| -----------------:| -------------:| --------------:| ------------------------------------:| ----------:|
-| Directory Extension   | right-aligned     | Application   | `user`,`group` | Extension_xxxxxxxxx_AttributeName    |            |
-| Open Extension        | centered          | Application   |                |         |         |
-| Schema Extension      |    $1             | Application   |                |         |         |
